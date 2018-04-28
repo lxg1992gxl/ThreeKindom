250538a6e0bed6eec429ab3b93bba02de2c204d8
Sulenna Sally Nicholson <sulenna.nicholson@anu.edu.au>
2018-04-29 02:49:43 +1000

    notes for and start on task 9

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -1,6 +1,10 @@
 package comp1110.ass2.gui;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Game extends Application {
@@ -13,8 +17,34 @@ public class Game extends Application {
 
     // FIXME Task 12: Integrate a more advanced opponent into your game
 
+    private static final int VIEWER_WIDTH = 933;
+    private static final int VIEWER_HEIGHT = 700;
+
+    private static final String URI_BASE = "assets/";
+
+    private final Group root = new Group();
+    private final Group controls = new Group();
+    private final StackPane board = new StackPane();
+    private final FlowPane flow = new FlowPane(7,7);    //to organize every card in grid
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Warring States");
+        Scene scene = new Scene(root, VIEWER_WIDTH, VIEWER_HEIGHT);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        //task 9
+        /** create board that shows current game state
+         * choose number of players
+         * display what cards have collected and kingdom flags
+         * allow to click on a card to attempt move
+         *      if move accepted, make move and take card/cards (make sure to take other cards as relevant)
+         *      if not accepted, error message
+         *  after move next player (indicate on screen which player's turn it is)
+         */
 
     }
 }
