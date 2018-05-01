4ffc2a661f84365b325b37c11a000149a82a0c09
Siying Qian <u6099927@anu.edu.au>
2018-05-02 01:17:49 +1000

    Add helper methods for the whole process to identify the final winner & add corresponding tests

--- src/comp1110/ass2/HelperMethodsForLaterTasks.java
+++ src/comp1110/ass2/HelperMethodsForLaterTasks.java
@@ -1,26 +1,23 @@
 package comp1110.ass2;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class HelperMethodsForLaterTasks {
 
-    // Task 7
-    public static String orderedCards(ArrayList<String> existedCards) {
-        char[] s_arr;
-        Collections.sort(existedCards);
-        s_arr = new char[2 * existedCards.size()];
-        for (int r = 0; r < existedCards.size(); r++) {
-            char[] r_arr = existedCards.get(r).toCharArray();
-            s_arr[2 * r] = r_arr[0];
-            s_arr[2 * r + 1] = r_arr[1];
-        }
-        return (new String(s_arr));
-    }
+//    // Task 7
+//    public static String orderedCards(ArrayList<String> existedCards) {
+//        char[] s_arr;
+//        Collections.sort(existedCards);
+//        s_arr = new char[2 * existedCards.size()];
+//        for (int r = 0; r < existedCards.size(); r++) {
+//            char[] r_arr = existedCards.get(r).toCharArray();
+//            s_arr[2 * r] = r_arr[0];
+//            s_arr[2 * r + 1] = r_arr[1];
+//        }
+//        return (new String(s_arr));
+//    }
 
 
     // Task 9
+    // get an array of the final flags number of every player, player ID will work as array index
     public static int[] playerFlagNumbers(int[] finalFlags) {
         // here, "int[] finalFlags = WarringStatesGame.getFlags(setup, moveSequence, numPlayers)" which we can get from Task 8 result
         int[] flagNumbers = new int[4];
@@ -34,9 +31,13 @@ public class HelperMethodsForLaterTasks {
     }
 
 
-    public static List<Integer> getMaximumFlagsIDs(int[] finalFlags) {
+    // get an array with player IDs of players who hold the maximum number of flags
+    public static int[] getMaximumFlagsIDs(int[] finalFlags) {
         // get No. of flags of every players
         int[] flagNumbers = playerFlagNumbers(finalFlags);
+
+        // initialize an array to record the player with the maximum flags
+        int[] maxFlagsPlayer = new int[] {-1, -1, -1, -1};
 
         // get the maximum No. of flags among all players
         int flagMax = 0;
@@ -47,10 +48,9 @@ public class HelperMethodsForLaterTasks {
         }
 
         // get all players IDs that own that max No. of flags
-        List<Integer> maxFlagsPlayer = new ArrayList<>();
         for (int i = 0; i < flagNumbers.length; i++) {
             if (flagNumbers[i] == flagMax) {
-                maxFlagsPlayer.add(i);
+                maxFlagsPlayer[i] = i;
             }
         }
 
@@ -58,47 +58,59 @@ public class HelperMethodsForLaterTasks {
     }
 
 
+    // get the final winner's ID
     public static int getWinnerID(int[] finalFlags) {
-        List<Integer> maxFlagsPlayer = getMaximumFlagsIDs(finalFlags);
+        int[] maxFlagsPlayer = getMaximumFlagsIDs(finalFlags);
 
         int winnerID = -1;
 
         // check whether there exists one only player holds the greatest number of flags
-        if (maxFlagsPlayer.size() == 1) {
-            winnerID = maxFlagsPlayer.get(0);
-        } else {
+        int p = 0;
+        int player = -1;
+        for (int i = 0; i < maxFlagsPlayer.length; i++) {
+            if (maxFlagsPlayer[i] != -1) {
+                player = i;
+                p++;
+            }
+        }
+
+        if (p == 1) {
+            winnerID = player;
+        }
+        else {
+
             // check whether there are player inside holds Qin flag
-            for (int i = 0; i < maxFlagsPlayer.size(); i++) {
-                if (maxFlagsPlayer.get(i) == finalFlags[0]) {
-                    winnerID = maxFlagsPlayer.get(i);
+            for (int i = 0; i < maxFlagsPlayer.length; i++) {
+                if (maxFlagsPlayer[i] == finalFlags[0]) {
+                    winnerID = maxFlagsPlayer[i];
                 }
             }
             // check whether there are player inside holds Qi flag
             if (winnerID == -1) {
-                for (int i = 0; i < maxFlagsPlayer.size(); i++) {
-                    if (maxFlagsPlayer.get(i) == finalFlags[1]) {
-                        winnerID = maxFlagsPlayer.get(i);
+                for (int i = 0; i < maxFlagsPlayer.length; i++) {
+                    if (maxFlagsPlayer[i] == finalFlags[1]) {
+                        winnerID = maxFlagsPlayer[i];
                     }
                 }
             }
             // check whether there are player inside holds Chu flag
             if (winnerID == -1) {
-                for (int i = 0; i < maxFlagsPlayer.size(); i++) {
-                    if (maxFlagsPlayer.get(i) == finalFlags[2]) {
-                        winnerID = maxFlagsPlayer.get(i);
+                for (int i = 0; i < maxFlagsPlayer.length; i++) {
+                    if (maxFlagsPlayer[i] == finalFlags[2]) {
+                        winnerID = maxFlagsPlayer[i];
                     }
                 }
             }
             // check whether there are player inside holds Zhao flag
             if (winnerID == -1) {
-                for (int i = 0; i < maxFlagsPlayer.size(); i++) {
-                    if (maxFlagsPlayer.get(i) == finalFlags[3]) {
-                        winnerID = maxFlagsPlayer.get(i);
+                for (int i = 0; i < maxFlagsPlayer.length; i++) {
+                    if (maxFlagsPlayer[i] == finalFlags[3]) {
+                        winnerID = maxFlagsPlayer[i];
                     }
                 }
             }
-
         }
+
         return winnerID;
     }
 
--- tests/comp1110/ass2/iTest.java
+++ tests/comp1110/ass2/iTest.java
@@ -1,14 +1,10 @@
 package comp1110.ass2;
 
-import comp1110.ass2.HelperMethodsForLaterTasks;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertFalse;
+import static junit.framework.TestCase.assertTrue;
 
 public class iTest {
     @Rule
@@ -16,48 +12,49 @@ public class iTest {
 
     @Test
     public void getPlayerFlagsNumber() {
-        // four-player condition
-        int[] flags1 = new int[]{1, 2, 3, 0, 1, 2, 3};
-        int[] nums1 = new int[]{1, 2, 2, 2};
-        assertFalse("Get all individual flags numbers correctly, but fail the test", HelperMethodsForLaterTasks.playerFlagNumbers(flags1) == nums1);
-        // two-player condition
-        int[] flags2 = new int[]{1, 0, 1, 1, 0, 1, 0};
-        int[] nums2 = new int[]{3, 4, 0, 0};
-        assertFalse("Get all individual flags numbers correctly, but fail the test", HelperMethodsForLaterTasks.playerFlagNumbers(flags2) == nums2);
+        // four-player condition (should get "int[] {1,2,2,2}")
+        int[] flags1 = new int[]{1, 2, 3, 1, 2, 3, 0};
+        assertTrue("should get individual flag number correctly, but was rejected", HelperMethodsForLaterTasks.playerFlagNumbers(flags1)[0] == 1);
+        assertTrue("should get individual flag number correctly, but was rejected", HelperMethodsForLaterTasks.playerFlagNumbers(flags1)[1] == 2);
+        // two-player condition (should get "int[]{3,3,0,0}")
+        int[] flags2 = new int[]{-1, 0, 1, 0, 0, 1, 1};
+        assertTrue("should get individual flag number correctly, but was rejected", HelperMethodsForLaterTasks.playerFlagNumbers(flags2)[1] == 3);
+        assertTrue("should get individual flag number correctly, but was rejected", HelperMethodsForLaterTasks.playerFlagNumbers(flags2)[2] == 0);
     }
 
     @Test
     public void getMaxFlagsPlayers() {
         // there exists a player who holds the greatest number of flags at the end
         int[] flags1 = new int[]{2, 1, 2, 2, 2, 0, 0};
-        List<Integer> lst1 = new ArrayList<>();
-        lst1.add(2);
-        assertFalse("Get players correctly, but fail the test", HelperMethodsForLaterTasks.getMaximumFlagsIDs(flags1) == lst1);
+        // (should get "int[] {-1,-1,2,-1}")
+        assertTrue("should get players correctly, but was rejected", HelperMethodsForLaterTasks.getMaximumFlagsIDs(flags1)[0] == -1);
+        assertTrue("should get players correctly, but was rejected", HelperMethodsForLaterTasks.getMaximumFlagsIDs(flags1)[3] == -1);
+        assertTrue("should get players correctly, but was rejected", HelperMethodsForLaterTasks.getMaximumFlagsIDs(flags1)[2] == 2);
         // there exist more than one players who hold the greatest number of flags
         int[] flags2 = new int[]{1, 2, 3, 1, 2, 3, 0};
-        List<Integer> lst2 = new ArrayList<>();
-        lst2.add(1);
-        lst2.add(2);
-        lst2.add(3);
-        assertFalse("Get players correctly, but fail the test", HelperMethodsForLaterTasks.getMaximumFlagsIDs(flags2) == lst2);
+        // (should get "int[] {-1,1,2,3}")
+        assertTrue("should get players correctly, but was rejected", HelperMethodsForLaterTasks.getMaximumFlagsIDs(flags2)[0] == -1);
+        assertTrue("should get players correctly, but was rejected", HelperMethodsForLaterTasks.getMaximumFlagsIDs(flags2)[2] == 2);
+        assertTrue("should get players correctly, but was rejected", HelperMethodsForLaterTasks.getMaximumFlagsIDs(flags2)[3] == 3);
         // exist flags that are not held by any player
         int[] flags3 = new int[]{-1, 0, 1, 0, 0, 1, 1};
-        List<Integer> lst3 = new ArrayList<>();
-        lst2.add(0);
-        lst2.add(1);
-        assertFalse("Get players correctly, but fail the test", HelperMethodsForLaterTasks.getMaximumFlagsIDs(flags3) == lst3);
+        // (should get "int[] {0,1,-1,-1}")
+        assertTrue("should get players correctly, but was rejected", HelperMethodsForLaterTasks.getMaximumFlagsIDs(flags3)[1] == 1);
     }
 
-//    @Test
-//    public void getWinnerIndex() {
-//        // there exists a player who holds the greatest number of flags at the end
-////        int[] flags1 = new int[]{2, 1, 2, 2, 2, 0, 0};
-////        assertFalse("Get the final winner correctly, but fail the test", HelperMethodsForLaterTasks.getWinnerID(flags1) == 2);
-//        // there exist more than one players who hold the greatest number of flags
-//        int[] flags2 = new int[]{1, 2, 3, 1, 2, 3, 0};
-//        assertFalse("Get the final winner correctly, but fail the test", HelperMethodsForLaterTasks.getWinnerID(flags2) == 1);
-//        int[] flags3 = new int[]{0, 2, 1, 1, 2, 1, 2};
-//        assertFalse("Get the final winner correctly, but fail the test", HelperMethodsForLaterTasks.getWinnerID(flags3) == 2);
-//    }
+    @Test
+    public void getWinnerIndex() {
+        // there exists a player who holds the greatest number of flags at the end
+        int[] flags1 = new int[]{2, 1, 2, 2, 2, 0, 0};
+        int winner1 = 2;
+        assertTrue("should get the final winner correctly, but was rejected", HelperMethodsForLaterTasks.getWinnerID(flags1) == winner1);
+        // there exist more than one players who hold the greatest number of flags
+        int[] flags2 = new int[]{1, 2, 3, 1, 2, 3, 0};
+        int winner2 = 1;
+        assertTrue("should get the final winner correctly, but was rejected", HelperMethodsForLaterTasks.getWinnerID(flags2) == winner2);
+        int[] flags3 = new int[]{0, 2, 1, 1, 2, 1, 2};
+        int winner3 = 2;
+        assertTrue("should get the final winner correctly, but was rejected", HelperMethodsForLaterTasks.getWinnerID(flags3) == winner3);
+    }
 
 }
