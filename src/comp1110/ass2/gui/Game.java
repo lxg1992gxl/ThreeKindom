e301bc490bd43c561506f1b53bb3e0144f04d97c
hypnus <u6464313@anu.edu.au>
2018-05-14 05:00:13 +1000

    Finished UI part, sent back three int and one boolean numberOfPlayer is the number of human players numberOfAI is the number of robot sum them up is the numberOfChairs, means how many opponents in this game advAI can be True or False, we can use it turn AI on and off.

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -2,20 +2,27 @@ package comp1110.ass2.gui;
 
 import comp1110.ass2.WarringStatesGame;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.SplitPane;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Popup;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
+import javafx.stage.WindowEvent;
+import sun.awt.SunHints;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import static comp1110.ass2.WarringStatesGame.*;
@@ -31,7 +38,7 @@ public class Game extends Application {
     private static final int VIEWER_WIDTH = 933;
     private static final int VIEWER_HEIGHT = 700;
 
-    private static final String URI_BASE = "img/";
+    private static final String URI_BASE = "assets/";
 
     private final Group root = new Group();
     private final Group controls = new Group();
@@ -43,11 +50,15 @@ public class Game extends Application {
     private final Group scrBD2 = new Group();
     private final Group scrBD3 = new Group();
 
+    int numberOfChairs;
+    int numberOfAI;
+    int numberOfPlayers;
+    boolean advAI;
 
     private String setup = "g0Aa0Bf1Ca1Dc5Ee1Fa4Ge3He2Ia2Jc2Kd0Lf0Mb4Nd4Oa6Pc3Qe0Ra5Sc1Td1Uc4Vb5Wb0Xa7Yf2Zb10a31z92b33b64d35g16b27d28c09";
     private String currentBoard = setup;
-    private int players = 4;
-    private int AIs = 1;
+    private int players = numberOfChairs;
+    private int AIs = numberOfAI;
     private String history = "";
     private int currentPlayer = 0;
     private FXpiece[][] cards = new FXpiece[players][35]; //number of players, number of character cards
@@ -66,7 +77,7 @@ public class Game extends Application {
             int loc = normaliseLoc(placement.charAt(2));
             int w = 100;
             int h = 100;
-            int gap = 3;
+            int gap = 5;
             setLayoutX(BOARD_WIDTH - ((loc / 6 + 1) * w + (loc / 6 + 1) * gap)); //inverse because starts on right
             setLayoutY(loc % 6 * h + loc % 6 * gap);
 
@@ -190,11 +201,170 @@ public class Game extends Application {
         }
     }
 
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        //
+        //setting window starts form here
+        Stage page1 = new Stage();
+        page1.setTitle("Choose the number of players!");
+        //build scene
+        Pane pane1 = new Pane();
+        Scene settingScene = new Scene(pane1, 350, 300);
+
+        //create buttons
+        Button exitBtn = new Button("Exit");
+        exitBtn.setLayoutX(50);
+        exitBtn.setLayoutY(258);
+        Button nextBtn = new Button("Next");
+        nextBtn.setLayoutX(220);
+        nextBtn.setLayoutY(258);
+        pane1.getChildren().addAll(exitBtn, nextBtn);
+
+        //event of buttons
+        exitBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                Event.fireEvent(page1, new WindowEvent(page1, WindowEvent.WINDOW_CLOSE_REQUEST));
+            }
+        });
+
+        nextBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                page1.hide();
+                Stage page2 = new Stage();
+                Pane pane2 = new Pane();
+                Scene scene = new Scene(pane2, 350, 300);
+                page2.setTitle("Add Robot");
+                page2.setScene(scene);
+                //create headline
+                Text headline = new Text("How many robot you wang to add?");
+                headline.setLayoutX(100);
+                headline.setLayoutY(120);
+                pane2.getChildren().add(headline);
+
+                //choicebox
+                ArrayList chsb = new ArrayList();
+                String list = "012345";
+                int i = 0;
+                while (i < numberOfChairs){
+                    chsb.add(list.substring(i, i+1));
+                    i++;
+                }
+                ChoiceBox choiceBox = new ChoiceBox();
+                choiceBox.setItems(FXCollections.observableArrayList(chsb));
+                choiceBox.setLayoutX(100);
+                choiceBox.setLayoutY(150);
+                pane2.getChildren().add(choiceBox);
+
+                choiceBox.getSelectionModel().selectedIndexProperty().addListener(((ov,oldv,newv)->{
+                    numberOfAI=newv.intValue();
+                }));
+
+                //checkbox
+                CheckBox checkBox = new CheckBox("Advanced AI");
+                checkBox.setLayoutX(200);
+                checkBox.setLayoutY(150);
+                pane2.getChildren().add(checkBox);
+                checkBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+                    public void changed(ObservableValue<? extends Boolean> ov,
+                                        Boolean old_val, Boolean new_val) {
+                        if (checkBox.isSelected()){
+                            advAI = true;
+                        }else{
+                            advAI = false;
+                        }
+                    }
+                });
+
+                //create buttons
+                Button backBtn = new Button("Back");
+                backBtn.setLayoutX(50);
+                backBtn.setLayoutY(258);
+                Button gameStartBtn = new Button("Game Start!");
+                gameStartBtn.setLayoutX(220);
+                gameStartBtn.setLayoutY(258);
+                pane2.getChildren().addAll(backBtn, gameStartBtn);
+
+                page2.show();
+
+                //set back button event
+                backBtn.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent e) {
+                        page2.hide();
+                        page1.show();
+                    }
+                });
+                //set start button event
+                gameStartBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        numberOfPlayers = numberOfChairs - numberOfAI;
+                        page2.hide();
+                        primaryStage.show();
+                        System.out.println(numberOfChairs);
+                        System.out.println(numberOfAI);
+                        System.out.println(numberOfPlayers);
+                        System.out.println(advAI);
+                    }
+                });
+
+            }
+        });
+
+
+        //create radio buttons group
+        ToggleGroup rb = new ToggleGroup();
+        RadioButton twoPlayers = new RadioButton("2-Players");
+        twoPlayers.setLayoutX(115);
+        twoPlayers.setLayoutY(145);
+        twoPlayers.setSelected(true);
+        twoPlayers.setToggleGroup(rb);
+        numberOfChairs = 2;
+
+
+        RadioButton threePlayers = new RadioButton("3-Players");
+        threePlayers.setLayoutX(115);
+        threePlayers.setLayoutY(180);
+        threePlayers.setToggleGroup(rb);
+
+        RadioButton fourPlayers = new RadioButton("4-Players");
+        fourPlayers.setLayoutX(115);
+        fourPlayers.setLayoutY(215);
+        fourPlayers.setToggleGroup(rb);
+
+        pane1.getChildren().addAll(twoPlayers, threePlayers, fourPlayers);
+
+        //create an int for store players number
+        twoPlayers.setUserData(2);
+        threePlayers.setUserData(3);
+        fourPlayers.setUserData(4);
+
+        rb.selectedToggleProperty().addListener(
+                (ObservableValue<? extends Toggle> ov, Toggle old_toggle, Toggle new_toggle) ->{
+                if (rb.getSelectedToggle() != null) {
+                    numberOfChairs = Integer.parseInt(rb.getSelectedToggle().getUserData().toString());
+                }
+
+        });
+
+        //create headline
+        Text headline = new Text("Game of Seven Kingdoms");
+        headline.setLayoutX(100);
+        headline.setLayoutY(70);
+        pane1.getChildren().add(headline);
+
+        //show scene
+        page1.setScene(settingScene);
+        page1.show();
+
         primaryStage.setTitle("Seven Kingdoms Game");
         Scene scene = new Scene(root, VIEWER_WIDTH, VIEWER_HEIGHT);
+        //
+        //setting window ends here
 
         makeBoard();
         makeScores();
@@ -211,7 +381,8 @@ public class Game extends Application {
         root.getChildren().addAll(board,cardCollectBoard);
 
         primaryStage.setScene(scene);
-        primaryStage.show();
+        //move "primaryStage.show" to setting window "GameStart Btn"
+
 
     }
 
@@ -235,5 +406,6 @@ public class Game extends Application {
 
 
     }
+
 }
 
