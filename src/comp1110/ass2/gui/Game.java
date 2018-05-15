c648d615cae4ef909dde238e2c93ecb7a819c4d1
Sulenna Sally Nicholson <sulenna.nicholson@anu.edu.au>
2018-05-15 12:35:38 +1000

    random AI core finished

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -56,7 +56,7 @@ public class Game extends Application {
     //    private String setup = "g0Aa0Bf1Ca1Dc5Ee1Fa4Ge3He2Ia2Jc2Kd0Lf0Mb4Nd4Oa6Pc3Qe0Ra5Sc1Td1Uc4Vb5Wb0Xa7Yf2Zb10a31z92b33b64d35g16b27d28c09";
     private String currentBoard = setup;
     //    private int players; // replace with numberOfPlayers
-    private boolean [] AI = new boolean[3]; //maximum number of AIs
+    private boolean [] AI = new boolean[4]; //maximum number of players
     private String history = "";
     private int currentPlayer = 0;
     private FXpiece[][] cards = new FXpiece[4][35]; //max number of players, number of character cards
@@ -106,7 +106,7 @@ public class Game extends Application {
                     showFlags();
 
                     //TODO if current player = AI, make next move based on AIstrategies here
-                    if(currentPlayer==1){
+                    if(AI[currentPlayer]){
                         AIMove(currentBoard);
                     }
 
@@ -157,12 +157,24 @@ public class Game extends Application {
         makeBoard();
         currentPlayer = (currentPlayer + 1) % numberOfPlayers;
         showFlags();
+        if(AI[currentPlayer]){
+            AIMove(currentBoard);
+        }
         //check for next player computer?
     }
 
     //TODO if current player = AI, make next move based on AIstrategies here
-    private void AIPlayer (int numberOfPlayers, int numberOfAI, int numberOfHumans){
+    private void AIPlayer (int numberOfPlayers, int numberOfHumans){
         //populate ai array with which playerIDs are AIs
+        for (int i = 0; i< numberOfPlayers; i++){
+            if (i<numberOfHumans){
+                AI[i] = false;
+
+            }
+            else {AI[i]=true;}
+            System.out.println(i + " is "+ AI[i]);
+        }
+
         //extension --> turn this array into an int array and allow AIs to have differnt difficulty levels
 
     }
@@ -435,12 +447,15 @@ public class Game extends Application {
 //                        System.out.println(advAI);
 //                        System.out.println(numberOfPlayers);
 //                        System.out.println(currentPlayer);
+
+                        //set up the AI players
+                        AIPlayer(numberOfPlayers, numberOfHumans);
+
                     }
                 });
 
             }
         });
-
 
         //show scene
         page1.setScene(settingScene);
