d0ba5f15faf4f9e4b46892ba0eba53666031b41a
Sulenna Sally Nicholson <sulenna.nicholson@anu.edu.au>
2018-05-15 10:44:17 +1000

    updates to task 11 AI to allow for variable number of AIs

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -56,6 +56,7 @@ public class Game extends Application {
     //    private String setup = "g0Aa0Bf1Ca1Dc5Ee1Fa4Ge3He2Ia2Jc2Kd0Lf0Mb4Nd4Oa6Pc3Qe0Ra5Sc1Td1Uc4Vb5Wb0Xa7Yf2Zb10a31z92b33b64d35g16b27d28c09";
     private String currentBoard = setup;
     //    private int players; // replace with numberOfPlayers
+    private boolean [] AI = new boolean[3]; //maximum number of AIs
     private String history = "";
     private int currentPlayer = 0;
     private FXpiece[][] cards = new FXpiece[4][35]; //max number of players, number of character cards
@@ -160,6 +161,12 @@ public class Game extends Application {
     }
 
     //TODO if current player = AI, make next move based on AIstrategies here
+    private void AIPlayer (int numberOfPlayers, int numberOfAI, int numberOfHumans){
+        //populate ai array with which playerIDs are AIs
+        //extension --> turn this array into an int array and allow AIs to have differnt difficulty levels
+        
+    }
+
 
     class Flag extends ImageView {
 
